9628c82800a583bd53e16b98b5d58eeecf713f1b
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-01-06 19:26:07 +0100

    Update products.js

--- models/products.js
+++ models/products.js
@@ -6,7 +6,7 @@ const  prodSchema  = new mongoose.Schema({
     title:String,
     userPosterId:{ type: Schema.Types.ObjectId, ref:'userSchema'}, // id user li habit l post
     description:String,
-   // isPub:Boolean,
+     isPub:Boolean,
     images:[],
     caractes:[],
     chat:[], /// kol produits aandou chat mte3ou ynijmou ykalmou akthir min wehed aala produit mte3ou,
