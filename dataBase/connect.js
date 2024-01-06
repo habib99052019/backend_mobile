dc5ce2bda155ad87c5f3241a129c8412c67a07d9
habib <ha9.0bib90@gmail.com>
2024-01-06 12:26:16 +0100

    first commit33

--- dataBase/connect.js
+++ dataBase/connect.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/backKoorea',
 //?retryWrites=true&w=majori
-mongoose.connect('mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/comm10002',
+mongoose.connect('mongodb+srv://habibfullstack90:suMi5SdKCbCXPyTx@cluster0.owwrwt9.mongodb.net/?retryWrites=true&w=majority', //mongodb+srv://webmaster:webmaster123@cluster0.jc0r9.mongodb.net/comm10002',
 {useNewUrlParser: true, 
 useUnifiedTopology: true,
 }).then(()=>console.log('Successfully connected to database.')).catch((e)=>console.error('Error in connection',e));
